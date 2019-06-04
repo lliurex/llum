ee063e6d9842d5e5ab2827f68420c5e33e756857
cless <hectorgh@gmail.com>
2019-06-04 16:23:02 +0200

    icons

--- llum/gtk-gui/MainWindow.cs
+++ llum/gtk-gui/MainWindow.cs
@@ -33,7 +33,7 @@ public partial class MainWindow
 		// Widget MainWindow
 		this.Name = "MainWindow";
 		this.Title = global::Mono.Unix.Catalog.GetString ("Llum - Lliurex User Manager");
-		//this.Icon = global::Stetic.IconLoader.LoadIcon (this, "llum", global::Gtk.IconSize.Menu);
+		this.Icon = global::Stetic.IconLoader.LoadIcon (this, "llum", global::Gtk.IconSize.Menu);
 		this.WindowPosition = ((global::Gtk.WindowPosition)(1));
 		// Container child MainWindow.Gtk.Container+ContainerChild
 		this.mainHBox = new global::Gtk.HBox ();
